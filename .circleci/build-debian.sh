3e755eb5a587ebadb0f5ec92533307253968753f
mgrouch <mgrushinskiy@gmail.com>
2021-06-02 21:52:44 -0400

    pc-navtex

---
+++ .circleci/build-debian.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+#
+# Build for Debian in a docker container
+#
+
+# bailout on errors and echo commands.
+set -xe
+
+DOCKER_SOCK="unix:///var/run/docker.sock"
+
+echo "DOCKER_OPTS=\"-H tcp://127.0.0.1:2375 -H $DOCKER_SOCK -s overlay2\"" | sudo tee /etc/default/docker > /dev/null
+sudo service docker restart
+sleep 5;
+
+if [ "$EMU" = "on" ]; then
+  if [ "$CONTAINER_DISTRO" = "raspbian" ]; then
+      docker run --rm --privileged multiarch/qemu-user-static:register --reset
+  else
+      docker run --rm --privileged --cap-add=ALL --security-opt="seccomp=unconfined" multiarch/qemu-user-static --reset --credential yes --persistent yes
+  fi
+fi
+
+WORK_DIR=$(pwd):/ci-source
+
+docker run --privileged --cap-add=ALL --security-opt="seccomp=unconfined" -d -ti -e "container=docker"  -v $WORK_DIR:rw $DOCKER_IMAGE /bin/bash
+DOCKER_CONTAINER_ID=$(docker ps --last 4 | grep $CONTAINER_DISTRO | awk '{print $1}')
+
+docker exec --privileged -ti $DOCKER_CONTAINER_ID apt-get update
+docker exec --privileged -ti $DOCKER_CONTAINER_ID apt-get -y install dpkg-dev debhelper devscripts pkg-config apt-utils fakeroot
+docker exec --privileged -ti $DOCKER_CONTAINER_ID apt-get -y install dh-exec autotools-dev autoconf gzip
+docker exec --privileged -ti $DOCKER_CONTAINER_ID apt-get -y install qt5-default libqt5serialport5-dev qt4-linguist-tools
+docker exec --privileged -ti $DOCKER_CONTAINER_ID /bin/bash -xec \
+    "update-alternatives --set fakeroot /usr/bin/fakeroot-tcp; cd ci-source; dpkg-buildpackage -b -uc -us -j4; mkdir dist; mv ../*.deb dist; chmod -R a+rw dist"
+
+find dist -name \*.\*$EXT
+
+echo "Stopping"
+docker ps -a
+docker stop $DOCKER_CONTAINER_ID
+docker rm -v $DOCKER_CONTAINER_ID
